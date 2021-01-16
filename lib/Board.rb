7caa6eb2b4f60e419e3be308ac442476219791d5
saimaAnis21 <saima_rehman21@yahoo.com>
2021-01-16 16:39:33 +0500

    Make classes Game , Board and Player

---
+++ lib/Board.rb
@@ -0,0 +1,15 @@
+class Board
+    def self.print_board(sym1=nil, sym2=nil, arr1=nil, arr2=nil)
+      (1..9).each do |i|
+        if arr1.include?(i)
+          print " #{sym1} "
+        elsif arr2.include?(i)
+          print " #{sym2} "
+        else  
+          print " #{i} "
+        end
+        print '|' unless (i % 3).zero?
+        puts "\n" if (i % 3).zero?
+      end
+    end
+  end
