d11a9af8baf38c8b6ba3335544a8fd5ba3b16149
saimaAnis21 <saima_rehman21@yahoo.com>
2021-01-16 16:54:29 +0500

    Remove Linter errors

--- lib/Player.rb
+++
@@ -1,7 +0,0 @@
-class Player
-    attr_accessor :name, :symbol, :moves
-    
-    def initialize
-      self.moves= []
-    end
-end
---
+++ lib/player_info.rb
@@ -0,0 +1,7 @@
+class Player
+  attr_accessor :name, :symbol, :moves
+
+  def initialize
+    self.moves = []
+  end
+end
