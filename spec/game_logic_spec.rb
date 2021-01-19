348eb8ec8fa64054095f4c3905e04311e2c5ad70
KumarAmitt <amitt2015@outlook.com>
2021-01-19 14:17:17 +0530

    Fix RubiCop Offenses

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -1,42 +1,42 @@
-require "game_logic"
+require 'game_logic'
 
 describe Game do
-    describe "::validmove" do
-        
-            it "should accept 2 arrays and an integer" do
-                expect {Game.validmove(2,[1,2,3],[4,5,6])}.to_not raise_error
-              end
-          
-              it "Check it returns false when move is already in an array" do
-                expect(Game.validmove(2,[1,2,3],[4,5,6])).to be_falsy
-              end
+  describe '::validmove' do
 
-              it "Check it true when move is not in any array" do
-                expect(Game.validmove(9,[1,2,3],[4,5,6])).to be_truthy
-              end
-
-              it "Check it returns false when move is not in 1-9" do
-                expect(Game.validmove(12,[1,2,3],[4,5,6])).to be_falsy
-              end
-        
-              it "Check it returns false when move is a character" do
-                expect(Game.validmove('asr',[1,2,3],[4,5,6])).to be_falsy
-              end
+    it 'should accept 2 arrays and an integer' do
+      expect { Game.validmove(2, [1, 2, 3], [4, 5, 6]) }.to_not raise_error
     end
 
-    describe "::checkwin" do
-
-        it "should accept 1 array and an integer" do
-            expect{Game.checkwin(2,[1,2,3])}.to_not raise_error
-          end
-
-        it "Check it returns true if the player has a winning move" do
-            expect(Game.checkwin(1,[1,2,3])).to be_truthy
-          end
-
-          it "Check it returns false if the player has no winning move" do
-            expect(Game.checkwin(2,[5,2,3])).to be_falsy
-          end
-
+    it 'Check it returns false when move is already in an array' do
+      expect(Game.validmove(2, [1, 2, 3], [4, 5, 6])).to be_falsy
     end
+
+    it 'Check it true when move is not in any array' do
+      expect(Game.validmove(9, [1, 2, 3], [4, 5, 6])).to be_truthy
+    end
+
+    it 'Check it returns false when move is not in 1-9' do
+      expect(Game.validmove(12, [1, 2, 3], [4, 5, 6])).to be_falsy
+    end
+
+    it 'Check it returns false when move is a character' do
+      expect(Game.validmove('asr', [1, 2, 3], [4, 5, 6])).to be_falsy
+    end
+  end
+
+  describe '::checkwin' do
+
+    it 'should accept 1 array and an integer' do
+      expect { Game.checkwin(2, [1, 2, 3]) }.to_not raise_error
+    end
+
+    it 'Check it returns true if the player has a winning move' do
+      expect(Game.checkwin(1, [1, 2, 3])).to be_truthy
+    end
+
+    it 'Check it returns false if the player has no winning move' do
+      expect(Game.checkwin(2, [5, 2, 3])).to be_falsy
+    end
+
+  end
 end
--- spec/player_info_spec.rb
+++ spec/player_info_spec.rb
@@ -1,14 +1,10 @@
-require "player_info"
+require 'player_info'
 
 describe Player do
-    subject(:plyr){Player.new}
-    describe "#initialize" do
-        
-        it 'should set the array as an empty array' do
-            
-            expect(plyr.instance_variable_get(:@moves)).to eq([])
-          end
-      
-
+  subject(:plyr){Player.new}
+  describe '#initialize' do
+    it 'should set the array as an empty array' do
+      expect(plyr.instance_variable_get(:@moves)).to eq([])
     end
+  end
 end
